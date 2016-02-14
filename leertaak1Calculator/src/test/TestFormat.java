01d28bd73cc9219087ee07a206f31370b995c5d0
Bas de Ruiter <b.j.de.ruiter@st.hanze.nl>
2016-02-14 14:20:56 +0100

    junit test voor octalbase

--- leertaak1Calculator/src/test/TestFormat.java
+++ leertaak1Calculator/src/test/TestFormat.java
@@ -38,7 +38,10 @@ public class TestFormat extends TestCase {
 			assertEquals("0.11",calc.secondOperand());
 			calc.setBase(new HexBase());
 			assertEquals("0.C",calc.secondOperand());
-
+			
+			calc.setBase(new OctalBase());
+			assertEquals("0.6",calc.secondOperand());
+			   
 			calc.setFormat(new FloatingPointFormat());
 			assertEquals("C.0*10^-1.0",calc.secondOperand());
 			calc.setBase(new BinaryBase());
